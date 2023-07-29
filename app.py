bc0b91c4baf38ba73d3d9a3245dfd20772d8bf01
sagarkumardse <b19138@students.iitmandi.ac.in>
2023-07-29 13:16:10 +0530

    Adding code

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+from Keyword import TextRank4Keyword
+from annotated_text import annotated_text
+from model import Summary
+from transformers import pipeline
+import json
+
+#https://www.youtube.com/watch?v=sNhhvQGsMEc
+def auto_format(text,keyword):
+        modified_text=[]
+        random_text = text.split()
+        for i in random_text:
+            if i in keyword and keyword[i] >= 1:
+                modified_text.append((i,'','#fafa'))
+            else:
+                modified_text.append(i+' ')
+        return modified_text
+
+def main(Model=None):
+    classifier = pipeline("summarization",model=Model,max_length = 130)
+    key_word_extractor = TextRank4Keyword()
+    splitted_trans,j = Summary.cleaning(link)
+    st.write("Started")
+    sample_text = splitted_trans[0]
+    #tabs = st.tabs(splitted_trans.keys())
+    for i in range(j):
+            
+        if len(splitted_trans[i][f's_{i}']) > 60:
+            with st.spinner('Generating summary...'):
+                summary = classifier(splitted_trans[i][f's_{i}'], do_sample=False)[0]['summary_text']
+                imp_words = key_word_extractor.analyze(summary)
+                
+                st.write("From : ",splitted_trans[i]['start'])
+                #splitted_trans[i]['summary'] = summary
+                #splitted_trans[i]['imp_words'] = imp_words
+                print(summary)
+                formatted = auto_format(summary,imp_words)
+                annotated_text(formatted)
+                #st.write(f"Percent Completed : {round(((i+1)/j)*100,2)}")
+                st.write("To : ",splitted_trans[i]['end'])
+        else:
+                classifier2 = pipeline("summarization",min_len=1)
+                summary = classifier2(splitted_trans[i][f's_{i}'], do_sample=False)[0]['summary_text']
+        st.success(f"Percent Completed : {round(((i+1)/j)*100,2)}")
+st.header("""
+Youtube Advanced Video Summarizer :sunglasses:
+""")
+
+link = st.text_input("Enter video ID or link here")
+if len(link):
+    url_starter = 'https://www.youtube.com/watch?v='
+    n = len(url_starter)
+    if link[:5] == 'https':
+        st.video(link)
+    else:
+        
+        st.video(url_starter + link)
+    models = ('Default','facebook/bart-large-cnn')
+    selected_model = st.selectbox(label = "Select Model", options = models,placeholder ='Select Model...')
+    if selected_model=='Default':
+        #classifier = pipeline("summarization",max_length = 130)
+        main()
+    else:
+        #classifier = pipeline("summarization",model=selected_model,max_length = 130)
+        main(selected_model)
+    st.write("Finished")
---
+++ model.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+from transformers import pipeline
+from youtube_transcript_api import YouTubeTranscriptApi
+from Keyword import TextRank4Keyword
+import streamlit as st
+class Summary():
+    def __init__(self,max_len = 100,min_len=60):
+        self.punc = '''-_?\/'''
+        self.max_len = 100
+        self.min_len=60
+
+        
+    def cleaning(link):
+        
+        #sample link = 'https://www.youtube.com/watch?v=DxL2HoqLbyA'
+        given_input = link
+        n = len( 'https://www.youtube.com/watch?v=')
+        if given_input[:5] == 'https':
+            video_ID = given_input[n:]
+        else:
+            video_ID = given_input
+        detailed_transcript = YouTubeTranscriptApi.get_transcript(video_ID)
+        N = len(detailed_transcript)
+        splitted_transcript = []                
+        #print(detailed_transcript[0],detailed_transcript[1])
+        i,j =0,0
+        while i<N-1:
+            start = detailed_transcript[i]['start']
+            tmp = ''
+            
+            while len(tmp)<950 and i < N-1:
+                temp = detailed_transcript[i]['text'].replace('-','')
+                temp = ' '.join(temp.splitlines())
+                tmp += temp
+                
+                i +=1
+                
+            #print(i,N)
+            end = detailed_transcript[i]['start'] + detailed_transcript[i]['duration']
+            dic = {f's_{j}' : tmp, 'start' :start, 'end' : end}
+            splitted_transcript.append(dic)
+            j +=1
+        return [splitted_transcript,j]
+    
+    def final_summaries(self,link):
+        #classifier = pipeline("summarization",model="facebook/bart-large-cnn")
+        classifier = pipeline("summarization")
+        key_word_extractor = TextRank4Keyword()
+        splitted_trans,j = self.cleaning(link)
+        print("Start")
+        sample_text = splitted_trans[0]
+        for i in range(j):
+            #print(len(splitted_trans[i][f's_{i}']))
+            if len(splitted_trans[i][f's_{i}']) > 60:
+                summary = classifier(splitted_trans[i][f's_{i}'], max_length=self.max_len, min_length=self.min_len, do_sample=False)[0]['summary_text']
+                imp_words = key_word_extractor.analyze(summary)
+                splitted_trans[i]['summary'] = summary
+                splitted_trans[i]['imp_words'] = imp_words
+            st.write(f'''
+            Percent Completed : {round(((i+1)/j)*100,2)}
+            ''')
+            #if i ==1:
+            #    break
+        print("Finish")    
+        return splitted_trans
